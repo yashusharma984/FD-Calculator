93dffaa1f9a19fdabfb43b986dc6fe3aa3c6071a
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-07-17 23:04:11 +0530

    commit - m

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculeMaturityAmount(){
+
+    // Get Input values from the form elements
+
+    const principle= parseFloat(document.getElementById('principle').value);
+    const interest= parseFloat(document.getElementById('interest').value);
+    const tenure= parseFloat(document.getElementById('tenure').value);
+
+
+    // perform the calculation
+
+    const maturityAmount = principle *(principle * interest *tenure)/100;
+
+    // Display the result
+
+    document.getElementById('result').innerText =`Maturity Amount : ${maturityAmount.toFixed(2)}`;
+}
+
+// Attach the event Listner to the Calulate Button
+document.getElementById('calculateBtn').addEventListener('click',calculeMaturityAmount);
