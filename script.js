343aab6dfb67d5baae8d60a684c5d6e8709c0aa0
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-07-17 23:26:41 +0530

    commit -m

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function calculateMaturityAmount(){
     // Get Input values from the form elements
 
     const principle= parseFloat(document.getElementById('principle').value);
-    const interest= parseFloat(document.getElementById('interest').value);
+    const intersrate= parseFloat(document.getElementById('interest').value);
     const tenure= parseFloat(document.getElementById('tenure').value);
 
 
     // perform the calculation
 
-    const maturityAmount = principle *(principle*interest*tenure)/100;
+    const maturityAmount = principle+(principle * intersrate *tenure)/100;
 
     // Display the result
 
-    document.getElementById('result').innerText =`Maturity Amount: ${maturityAmount.toFixed(2)}`;
+    document.getElementById('results').innerText =`Maturity Amount: ${maturityAmount.toFixed(2)}`;
 }
 
 // Attach the event Listner to the Calulate Button
